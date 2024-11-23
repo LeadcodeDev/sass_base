64f58eda78060049dc3b9ef1a9e304bc57126b84
Thibault <thibault.pointurier.pro@gmail.com>
2024-11-23 23:39:00 +0100

    feat(users): implement migration

--- database/migrations/1732378629907_create_users_table.ts
+++ database/migrations/1732378629907_create_users_table.ts
@@ -5,11 +5,13 @@ export default class extends BaseSchema {
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id').notNullable()
-      table.string('full_name').nullable()
+      table.string('id').notNullable().primary()
+      table.string('first_name').notNullable()
+      table.string('last_name').notNullable()
       table.string('email', 254).notNullable().unique()
       table.string('password').notNullable()
-
+      table.enum('type', ['user', 'practitioner', 'staff']).notNullable().defaultTo('user')
+      table.boolean('is_active').notNullable().defaultTo(true)
       table.timestamp('created_at').notNullable()
       table.timestamp('updated_at').nullable()
     })
