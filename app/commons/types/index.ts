b9a77533ef1006769bf160dd864fab41c3d1bbe1
Thibault <thibault.pointurier.pro@gmail.com>
2024-12-01 09:16:13 +0100

    feat(users): add avatar

---
+++ app/commons/types/index.ts
@@ -0,0 +1,11 @@
+export type UploadedFile = {
+  clientName: string
+  extname: string
+  fileName: string
+  tmpPath?: string
+  isValid: boolean
+  size: number
+  type: string
+  subtype: string
+  move: (location: string, options?: { name?: string }) => Promise<{ fileName: string }>
+}
