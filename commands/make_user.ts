ae37cbc1934ec8c3f059f4e15790a09362efb8e1
bourgu20al <bourgu20al@lycee-ndduroc.com>
2025-01-08 09:17:34 +0100

    feat: create new make:user command

---
+++ commands/make_user.ts
@@ -0,0 +1,39 @@
+import { BaseCommand } from '@adonisjs/core/ace'
+import type { CommandOptions } from '@adonisjs/core/types/ace'
+
+export default class MakeUser extends BaseCommand {
+  static commandName = 'make:user'
+  static description = 'Create a new User'
+  static help = ['Requires a lastname, firstname, email and password']
+  static options: CommandOptions = {
+    startApp: true,
+  }
+
+  async interact() {
+    this.logger.info('Creating user')
+  }
+
+  async run() {
+    const { default: User } = await import('#app/commons/models/User')
+
+    try {
+      const firstname = await this.prompt.ask('Enter the firstname')
+      const lastname = await this.prompt.ask('Enter the lastname')
+      const email = await this.prompt.ask('Enter the email')
+      const password = await this.prompt.ask('Enter the password')
+
+      await User.create({
+        firstname: firstname,
+        lastname: lastname,
+        email: email,
+        password: password,
+      })
+
+      this.logger.success('Successfully created user')
+    } catch (error) {
+      this.logger.error(error.message)
+      this.error = error
+      this.exitCode = 1
+    }
+  }
+}
